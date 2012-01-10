c28a55f7c23e250f0e984713fdbf02540f9a0c06
David Bradford <dbradf@gmail.com>
2012-01-10 10:35:56 -0600

    Updated command prompt

--- bash/plugins/prompt.bash
+++ bash/plugins/prompt.bash
@@ -1,6 +1,9 @@
 
 load_colors
 
+
+# See http://www.heartmindcode.com/blog/2011/03/crazy-bash-programming-with-wayne-e-seguin/
+
 ps1_git()
 {
     local branch=""
@@ -8,9 +11,23 @@ ps1_git()
     branch=`git_get_current_branch`
 
     if [ "$branch" != "" ]; then
-        printf "(git:$branch)"
+        printf "${YELLOWF}(git:$branch)${COLORRESET}"
     fi
 } # end ps1_git()
+
+
+ps1_identity()
+{
+    if [[ $UID -eq 0 ]]; then
+        printf "${REDF}\u${COLORRESET}"
+    else
+        printf "${GREENF}\u${COLORRESET}"
+    fi
+
+    printf "@${CYANF}\h${COLORRESET}:${GREENF}\w${COLORRESET} "
+
+    return 0
+} # end ps1_identity()
 
 
 ps1_error()
@@ -18,14 +35,47 @@ ps1_error()
     local RC=$?
 
     if [ $RC -ne 0 ]; then
-        printf "($RC) "
+        printf "${WHITEF}${REDB}($RC)${COLORRESET} "
     fi
 } # end ps1_error()
 
 
 ps1_set()
 {
-    PS1="${REDF}\$(ps1_error)${CYANF}[\h] ${GREENF}\w ${YELLOWF}\$(ps1_git)${COLORRESET}\n${CYANF}\$${COLORRESET} "
+    local prompt_char='$'
+    local separator="\n"
+
+    if [[ $UID -eq 0 ]]; then
+        prompt_char='#'
+    fi
+
+    while [[ $# -gt 0 ]]; do
+        token="$1"; shift
+
+        case "$token" in
+            --trace)
+                export PS4='+[${BASH_SOURCE}] : ${LINENO} : ${FUNCNAME[0]:+${FUNCNAME[0]}() $ }'
+                shopt -o xtrace
+                ;;
+
+            --prompt)
+                prompt_char="$1"
+                shift
+                ;;
+
+            --separator)
+                separator="$1"
+                shift
+                ;;
+
+            *)
+                true # Ignore everything else
+                ;;
+        esac
+    done
+
+    PS1="\$(ps1_error)$(ps1_identity) \$(ps1_git)${separator}${prompt_char} "
 } # end ps1_set()
 
-ps1_set
+
+ps1_set --prompt ">"
