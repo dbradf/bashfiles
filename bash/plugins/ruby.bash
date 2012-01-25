267401a3f9788a131e94f21000bde9f328ebabed
David Bradford <dbradf@gmail.com>
2012-01-25 12:18:46 -0600

    Don't use rbenv on aix.

--- bash/plugins/ruby.bash
+++ bash/plugins/ruby.bash
@@ -2,11 +2,9 @@
 # Add rbenv to the start of the path
 if [ "$SYSTEM" = "Darwin" ]; then
     export PATH="$HOME/.rbenv/bin:$PATH"
+    eval "$(rbenv init -)"
 elif [ "SYSTEM" = "AIX" ]; then
     export PATH=$PATH:$HOME/local/stow/ruby-1.9.3-p0/bin
 fi
-
-
-eval "$(rbenv init -)"
 
 alias man="gem man -s"
